e703fe0e242a71a400a18d38b2cc1e3f4217000d
Hakki <hakki.sahinkaya@gmail.com>
2024-04-30 21:42:31 +0100

    Make "Ball" an Area2D

--- Game2_Breakout/scenes/ball/Ball.cs
+++ Game2_Breakout/scenes/ball/Ball.cs
@@ -1,23 +1,24 @@
 using Godot;
 
-public partial class Ball : RigidBody2D
+public partial class Ball : Area2D
 {
-    public override void _Ready()
+    private Vector2 _velocity = new(0, 600);
+
+    public override void _PhysicsProcess(double delta)
     {
-        LinearVelocity = new Vector2(0, 600);
+        Position += _velocity * (float)delta;
     }
 
-    public void OnBallBodyEntered(Node body)
+    public void OnBallBodyEntered(Node2D area)
     {
         GD.Print("paddle");
 
-        if (body is not Paddle paddle) return;
-        
-        
+        if (area is not Paddle paddle) return;
+
         // Calculate new direction based on where the ball hit the paddle
         var hitPos = (GlobalPosition.X - paddle.GlobalPosition.X) /
                      (paddle.GetNode<Sprite2D>("Sprite2D").Texture.GetWidth() / 2);
         // Increase the speed of the ball after it hits the paddle
-        LinearVelocity = new Vector2(hitPos, -1).Normalized() * 500; // Adjust this value as needed
+        _velocity = new Vector2(hitPos, -1).Normalized() * 500; // Adjust this value as needed
     }
 }
